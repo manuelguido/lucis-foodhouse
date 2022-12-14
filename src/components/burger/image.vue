834f5e1dd16fab83594faaf0a9b4ddc79a352e70
manuelguido <manuelguido.m@gmail.com>
2022-12-14 17:20:20 -0300

    Refactored burger image

---
+++ src/components/burger/image.vue
@@ -0,0 +1,22 @@
+<template>
+  <img class="burger-image" :src="require(`@/assets/burgers/${path}`)" />
+</template>
+
+<script>
+export default {
+  name: 'BurgerImage',
+  props: {
+    path: {
+      type: String,
+      default: '', 
+    },
+  }
+}
+</script>
+
+<style scoped>
+.burger-image {
+  width: 105% !important;
+  border-radius: 16px;
+}
+</style>
--- src/components/burger/index.vue
+++ src/components/burger/index.vue
@@ -1,52 +1,43 @@
 <template>
-  <mdbCol class="burger animated fadeIn mb-5" col=12 lg=4 @click.native="loadModal">
+  <mdbCol class="mb-5" col=12 lg=4 @click.native="loadModal">
+    <div class="burger animated fadeIn">
     <mdbRow>
-      <!-- Image -->
       <mdbCol col=4>
-        <img class="burger-image" :src="require(`@/assets/burgers/${burger.image}`)" />
+        <BurgerImage :path="burger.image"/>
       </mdbCol>
-      <!-- Description -->
       <mdbCol col=8 class="pl-4">
         <h2
           class="burger-title my-lg-3"
           :class="burger.font"
           :style="'font-size:' + burger.size + 'em;'"
-        >{{burger.name}}</h2>
+        >
+          {{burger.name}}
+        </h2>
         <p class="burger-ingredients majorm black-a">{{burger.ingredients}}</p>
       </mdbCol>
     </mdbRow>
+    </div>
   </mdbCol>
 </template>
 
 <script>
 import { mdbRow, mdbCol } from 'mdbvue';
-// import burgerModal from '@/components/BurgerModal.vue'
-// import Vue from 'vue'
+import BurgerImage from '@/components/burger/image.vue'
 
 export default {
   name: 'Burger',
   components: {
     mdbRow,
-    mdbCol
+    mdbCol,
+    BurgerImage
     },
   props: {
-    burger: Object
+    burger: Object,
+    default: () => {},
   },
   data () {
     return {
       display_modal: false
-    }
-  },
-  methods: {
-    loadModal: function (event) {
-      if (event) {
-        this.display_modal = true
-        // var ComponentClass = Vue.extend(burgerModal)
-        // var instance = new ComponentClass()
-        // instance.$mount()
-        // var container = document.querySelector('#container-' + this.burger.id)
-        // container.appendChild(instance.$el)
-      }
     }
   }
 }
@@ -56,15 +47,13 @@ export default {
 .burger {
   cursor: pointer;
   transition: 0.1s all;
+  border-radius: 16px;
 }
 .burger:hover {
   background: #f8f8f8;
 }
 .burger:active {
   background: #f1f1f1;
-}
-.burger-image {
-  width: 105% !important;
 }
 .burger-title {
   text-shadow: 0px 3px 6px rgba(0,0,0,0.16);
